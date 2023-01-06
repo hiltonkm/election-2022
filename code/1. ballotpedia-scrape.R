75122c3f21416b4dc70d3c71cc5ce6d55f2d8f89
hiltonkm <katherine.mae.hilton@gmail.com>
2023-01-05 23:58:25 -0500

    setting up code

---
+++ code/1. ballotpedia-scrape.R
@@ -0,0 +1,12 @@
+# GOAL: Scrape data from ballotpedia
+
+## Note: using this to get more comfortable with web scraping in R
+## Source: https://www.dataquest.io/blog/web-scraping-in-r-rvest/
+## Ballotpedia: https://ballotpedia.org/United_States_Senate_elections,_2022
+
+# Libraries --------------------------------------------------------------------
+rm(list=ls())
+
+library(tidyverse)
+
+
